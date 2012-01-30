4cdedf0e9c99b64b174c0c4e01ed39ba40ed6259
Belden Lyman <belden@shutterstock.com>
2012-01-30 14:11:39 -0500

    add a MANIFEST; version bump

--- lib/Hash/MostUtils.pm
+++ lib/Hash/MostUtils.pm
@@ -6,7 +6,7 @@ use warnings;
 use base qw(Exporter);
 
 our @EXPORT_OK = qw(hashmap hashgrep hashapply n_map n_grep n_apply lvalues lkeys);
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 =head1 NAME
 
